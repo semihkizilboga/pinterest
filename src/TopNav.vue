fce51a612f5a442b0a6795fbb19cb5bacfca911d
Semih Kızılboğa <78806541+semihkizilboga@users.noreply.github.com>
2021-09-14 20:39:10 +0300

    Update TopNav.vue

--- src/TopNav.vue
+++ src/TopNav.vue
@@ -12,10 +12,26 @@
         <span class="material-icons absolute mt-3 ml-3 text-muted">search</span>
         <input class="w-full hover:bg-lightHover rounded-full bg-light py-3 pr-3 pl-10" placeholder="Search" type="text"  />
       </div>
+     <div class="flex items-center">
+       <button type="button" name="button" class="relative rounded-full hover:bg-light flex items-center justify-center p-3">
+         <span class="material-icons text-3xl text-muted">notifications</span>
+         <div class="rounded-full h-5 w-5 bg-primary items-center flex justify-center text-white absolute mb-5 ml-5"> 4 </div>
+       </button>
+       <button type="button" name="button" class="relative rounded-full hover:bg-light flex items-center justify-center p-3">
+         <span class="material-icons text-3xl text-muted">textsms</span>
+         <div class="rounded-full h-5 w-5 bg-primary items-center flex justify-center text-white absolute mb-5 ml-5"> 31 </div>
+       </button>
+       <button type="button" name="button" class="relative rounded-full hover:bg-light flex items-center justify-center p-3">
+        <img src="./assets/profile.jpg" class="h-6 w-6 rounded-full" alt="">
+       </button>
+       <button type="button" name="button" class="w-6 h-6 hover:bg-light rounded-full flex items-center justify-center">
+         <span class="material-icons text-muted">expand_more</span>
+       </button>
+      </div>
     </div>
 </template>
 <script>
-export default { 
+export default {
     data() {
         return {
             tabItems: [
